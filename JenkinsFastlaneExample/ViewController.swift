b20c8aa5caa64070406ecb1e6d67126b8d4ad196
Raúl Pérez Gómez <raul@Rauls-Mac-mini.local>
2019-06-16 22:01:14 +0200

    -

--- JenkinsFastlaneExample/ViewController.swift
+++ JenkinsFastlaneExample/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Christoph Herrmann on 31/03/2017.
 //  Copyright © 2017 Christoph Herrmann. All rights reserved.
-//
+// This is a comment
 
 import UIKit
 
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
